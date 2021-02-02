44060b23618436511bfd7e7d52d60cc5d0e47fb9
jakub.majkowski.stud@pw.edu.pl <=KsC=jy>
2021-02-02 07:48:58 +0100

    poprawny sposób animacji

--- src/prozeman/GameWindow.java
+++ src/prozeman/GameWindow.java
@@ -5,6 +5,9 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class GameWindow extends JPanel {
     private int difficulty;
@@ -47,17 +50,54 @@ public class GameWindow extends JPanel {
         pellets = 0;
         level = 0;
         playing = false;
+
         try {
             Config.loadConfig();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //addKeyListener(new TAdapter());
+
         setFocusable(true);
         setBackground(Color.darkGray);
         map = Config.maps.get(level);
         decodeMap();
+        pacman = new Pacman(cellSize);
+        pacman.setDirection(Direction.STOP);
+        loadDifficulty();
+
+        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("UP"), Direction.UP);
+        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("DOWN"), Direction.DOWN);
+        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("LEFT"), Direction.LEFT);
+        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("RIGHT"), Direction.RIGHT);
+        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("CONTROL"), Direction.STOP);
+        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("SPACE"), "START");
+        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("P"), "PAUSE");
+        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("ESCAPE"), "PAUSE");
+
+
+        this.getActionMap().put(Direction.UP, new MoveAction(Direction.UP));
+        this.getActionMap().put(Direction.DOWN, new MoveAction(Direction.DOWN));
+        this.getActionMap().put(Direction.LEFT, new MoveAction(Direction.LEFT));
+        this.getActionMap().put(Direction.RIGHT, new MoveAction(Direction.RIGHT));
+        this.getActionMap().put(Direction.STOP, new MoveAction(Direction.STOP));
+        this.getActionMap().put("START", new MiscAction("START"));
+        this.getActionMap().put("PAUSE", new MiscAction("PAUSE"));
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                animation();
+            }
+        };
+
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(runnable, 0, 10, TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * Metoda wczytująca odpowiednie liczby związane z poziomem trudności oraz tworząca odpowiednią liczbę duszków
+     */
+    private void loadDifficulty() {
         int nGhosts;
         switch (difficulty) {
             case 1: //easy
@@ -84,32 +124,10 @@ public class GameWindow extends JPanel {
                 lives = Config.numberOfLives_easy;
                 nGhosts = Config.numberOfGhosts_easy;
         }
-
-
-        pacman = new Pacman(cellSize);
         ghosts = new Ghost[nGhosts];
         for (int i = 0; i < ghosts.length; i++) {
             ghosts[i] = new Ghost(cellSize);
         }
-        pacman.setDirection(Direction.STOP);
-
-        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("UP"), Direction.UP);
-        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("DOWN"), Direction.DOWN);
-        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("LEFT"), Direction.LEFT);
-        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("RIGHT"), Direction.RIGHT);
-        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("CONTROL"), Direction.STOP);
-        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("SPACE"), "START");
-        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("P"), "PAUSE");
-        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("ESCAPE"), "PAUSE");
-
-        this.getActionMap().put(Direction.UP, new MoveAction(Direction.UP));
-        this.getActionMap().put(Direction.DOWN, new MoveAction(Direction.DOWN));
-        this.getActionMap().put(Direction.LEFT, new MoveAction(Direction.LEFT));
-        this.getActionMap().put(Direction.RIGHT, new MoveAction(Direction.RIGHT));
-        this.getActionMap().put(Direction.STOP, new MoveAction(Direction.STOP));
-        this.getActionMap().put("START", new MiscAction("START"));
-        this.getActionMap().put("PAUSE", new MiscAction("PAUSE"));
-
     }
 
     /**
@@ -174,6 +192,9 @@ public class GameWindow extends JPanel {
         }
     }
 
+    /**
+     * Metoda wprawiająca w ruch obiekty na ekranie
+     */
     public void animation() {
         if (playing) {
             for (int i = 0; i < ghosts.length; i++) {
@@ -202,6 +223,10 @@ public class GameWindow extends JPanel {
         }
     }
 
+    /**
+     * Metoda kontrolująca przechodzenie poszczególnych poziomów rozgrywki.
+     * Gdy gracz ukończy ostatnią planszę, gra zostanie zakończona a tu zostanie obliczony jego wynik
+     */
     private void passedLevel() {
         pellets = 1;
         if (level >= Config.numberOfLevels - 1) {
@@ -234,6 +259,10 @@ public class GameWindow extends JPanel {
         }
     }
 
+    /**
+     * Metoda kontrolująca ubywające życia po zderzeniu z duszkami.
+     * Gdy życia kończą się, gra zostaje przerwana
+     */
     private void loseLife() {
         lives--;
         if (lives <= 0) {
@@ -247,6 +276,9 @@ public class GameWindow extends JPanel {
         pause();
     }
 
+    /**
+     * Metoda zatrzymująca czas gry
+     */
     private void pause() {
         prevTime = elapsedTime;
         pacman.stop();
@@ -258,6 +290,11 @@ public class GameWindow extends JPanel {
         playing = false;
     }
 
+    /**
+     * Metoda pozwalająca wyświetlić wiadomość startową na ekranie
+     * @param g2d - kontekst graficzny
+     * @param message - treść wiadomości
+     */
     private void showMessage(Graphics2D g2d, String message) {
 
         g2d.setFont(Config.fontLarge);
@@ -283,8 +320,8 @@ public class GameWindow extends JPanel {
         Rectangle boardRect = new Rectangle((screenWidth / 2) - 300, (screenHeight / 2) - 300, 600, 600);
         g2d.fill(boardRect);
 
-        int cellWidth = cellSize;//boardRect.width / 20;
-        int cellHeight = cellSize;//boardRect.height / 20;
+        int cellWidth = cellSize;
+        int cellHeight = cellSize;
 
         boardRectIncX = boardRect.x;
         boardRectIncY = boardRect.y;
@@ -368,8 +405,8 @@ public class GameWindow extends JPanel {
         //rysowanie numeru poziomu
         g2d.drawString("Level " + Integer.toString(level + 1), (screenWidth / 2) - 50, hy + 25);
 
+        Toolkit.getDefaultToolkit().sync();
         g2d.dispose();
-        animation();
     }
 
     /**
@@ -415,46 +452,9 @@ public class GameWindow extends JPanel {
         return false;
     }
 
-  /*  class TAdapter extends KeyAdapter {
-        @Override
-        public void keyPressed(KeyEvent e) {
-
-            int key = e.getKeyCode();
-
-            if (playing) {
-                pacman.start();
-                switch(key) {
-                    case KeyEvent.VK_LEFT:
-                        pacman.setDirection(Direction.LEFT);
-                        break;
-                    case KeyEvent.VK_RIGHT:
-                        pacman.setDirection(Direction.RIGHT);
-                        break;
-                    case KeyEvent.VK_UP:
-                        pacman.setDirection(Direction.UP);
-                        break;
-                    case KeyEvent.VK_DOWN:
-                        pacman.setDirection(Direction.DOWN);
-                        break;
-                    case KeyEvent.VK_CONTROL:
-                        pacman.setDirection(Direction.STOP);
-                        break;
-                    case KeyEvent.VK_P:
-                        pause();
-                        break;
-                    default:
-                        break;
-                }
-            } else {
-                if (key == KeyEvent.VK_SPACE) {
-                    playing = true;
-                    startTime = System.nanoTime();
-                }
-            }
-            repaint();
-        }
-    }*/
-
+    /**
+     * Klasa obsługująca akcje związane z ruchem Pacmana
+     */
     private class MoveAction extends AbstractAction {
 
         Direction direction;
@@ -474,6 +474,9 @@ public class GameWindow extends JPanel {
         }
     }
 
+    /**
+     * Klasa obsługująca pozostałe akcje wywołane przez gracza
+     */
     private class MiscAction extends AbstractAction {
 
         String action;
