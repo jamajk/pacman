06ca70c7664dd6259ceb3e1bf4b081ce3b6125d1
jakub.majkowski.stud@pw.edu.pl <=KsC=jy>
2021-04-01 17:10:22 +0200

    Centered text in HowToPlay screen

--- src/prozeman/HowToPlayScreen.java
+++ src/prozeman/HowToPlayScreen.java
@@ -26,16 +26,29 @@ public class HowToPlayScreen extends JPanel {
 
         JLabel textLabel1 = new JLabel();
         textLabel1.setFont(Config.fontSmall);
+        textLabel1.setHorizontalAlignment(SwingConstants.CENTER);
+        textLabel1.setAlignmentX(CENTER_ALIGNMENT);
         textLabel1.setForeground(Color.white);
         textLabel1.setText("<html>The goal of the game is to eat the pellets<br>and score points.<br>This task is made harder by ghosts<br>roaming around and taking your lives.</html>");
         add(textLabel1);
         add(Box.createVerticalStrut(20));
 
-        JLabel textLabel2 = new JLabel();
+        JLabel textLabel2 = new JLabel("Controls:", JLabel.CENTER);
         textLabel2.setFont(Config.fontSmall);
+        textLabel2.setHorizontalAlignment(SwingConstants.CENTER);
+        textLabel2.setAlignmentX(CENTER_ALIGNMENT);
+        textLabel2.setHorizontalTextPosition(SwingConstants.CENTER);
         textLabel2.setForeground(Color.white);
-        textLabel2.setText("<html>Controls:<br><br>Arrow keys to move.<br>Ctrl to stop your character.<br>Press P or Escape to pause.</html>");
         add(textLabel2);
+        add(Box.createVerticalStrut(20));
+
+        JLabel textLabel3 = new JLabel("<html>Arrow keys to move.<br>Ctrl to stop your character.<br>Press P or Escape to pause.</html>", JLabel.CENTER);
+        textLabel3.setFont(Config.fontSmall);
+        textLabel3.setHorizontalAlignment(SwingConstants.CENTER);
+        textLabel3.setAlignmentX(CENTER_ALIGNMENT);
+        textLabel3.setHorizontalTextPosition(SwingConstants.CENTER);
+        textLabel3.setForeground(Color.white);
+        add(textLabel3);
         add(Box.createVerticalStrut(20));
 
         TextButton backButton = new TextButton("Back");
